4ddb1750852636fbdd279ea2769bff6f26e77efc
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-07-30 11:48:36 -0500

    added update folderName to console.log

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
 import NotefulForm from '../NotefulForm/NotefulForm'
+import config from '../config'
 
+// fetch(`${config.API_ENDPOINT}/folders/`, {
+//   method: 'POST',
+//   headers: {
+//     'content-type': 'application/json'
+//   },
+// })
 export default class AddFolder extends Component {
   render () {
     return (
--- src/NotefulForm/NotefulForm.js
+++ src/NotefulForm/NotefulForm.js
@@ -1,17 +1,74 @@
-import React from 'react'
+// import React from 'react'
 import './NotefulForm.css'
+import React, { Component } from 'react'
+//import './';
+//import  from './';
 
-export default function NotefulForm(props) {
-  const { className, ...otherProps } = props
-  return (
-    <form
-      className={['Noteful-form', className].join(' ')}
-      action='#'
-      {...otherProps}
-    >
-      <label htmlFor="folderName">Folder Name: </label>
-      <input type="text" defaultValue="MyFolderName" name="folderName"></input>
-      <input type="submit"></input>
-    </form>
-  )
+//import './';
+//import  from './';
+
+
+class NotefulForm extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      folderName : {
+            value:""
+        },
+      noteNote : {
+          value:""
+        },
+      validateFolder: {
+          value: "",
+        }
+    };
+  }
+
+// ValidateFolder= ()=>{
+//   const name=this.
+// }
+updateFolderName= (folderName)=>{    
+ 
+  this.setState(
+    {folderName: {value: folderName}}
+  ) 
+  console.log(this.state.folderName)
 }
+  render() {
+
+
+
+    // fetch(`${config.API_ENDPOINT}/notes/${noteId}`, {
+    //   method: 'POST',
+    //   headers: {
+    //     'content-type': 'application/json'
+    //   },
+    // })
+    const { className, ...otherProps } = this.props
+
+    return(
+    <div className='container'>
+      <form
+        className={['Noteful-form', className].join(' ')}
+        action='#'
+        {...otherProps}
+      > 
+        <label htmlFor="folderName">Folder Name: </label>
+        <input 
+          type="text" 
+          defaultValue="MyFolderName" 
+          name="folderName"
+          onChange={e => this.updateFolderName(e.target.value)}
+        >
+        </input>
+      
+        <input type="submit"></input>
+      </form> 
+    </div>
+    );
+  }
+}
+
+export default NotefulForm
+
